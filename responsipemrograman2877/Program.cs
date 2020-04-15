443dc555864ae12ae3fdadcbd7c1781ac8417b30
rz27 <Feizalreza29@gmail.com>
2020-04-15 15:30:40 +0700

    responsi

---
+++ responsipemrograman2877/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace responsipemrograman2877
+
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Karyawan karyawan1= new Karyawan (1, 19112877, "reza", 8000000);
+            karyawan1.gaji();
+            Karyawan karyawan2 = new Karyawan(2, 1911289, "aniq ", 9000000);
+            karyawan2.gaji();
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" GAJI NAIK 10%!");
+            Console.WriteLine(" ");
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Karyawan karyawan3 = new Karyawan(1, 19112877, "reza", 8000000);
+            karyawan3.gajinaik();
+            Karyawan karyawan4 = new Karyawan(2, 1911289, "aniq ", 9000000);
+            karyawan4.gajinaik();
+        }
+    }
+}
---
+++ responsipemrograman2877/karyawan.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsipemrograman2877 {
+    public class Karyawan {
+        public Karyawan(int no, int nik, string nama, int gb) {
+            No = no;
+            Nik = nik;
+            Nama = nama;
+            GB = gb;
+        }
+
+        public int No {
+            get;
+            set;
+        }
+
+        public int Nik {
+            get;
+            set;
+        }
+
+        public string Nama {
+            get;
+            set;
+        }
+
+        public int GB {
+            get;
+            set;
+        }
+
+        public void gaji() {
+            Console.WriteLine("{0}. {1} {2} {3}", No, Nik, Nama, GB);
+
+            if (GB < 0) {
+                Console.WriteLine("Maaf Gaji Kurang dari 0");
+            }
+        }
+
+        public void gajinaik() {
+            Console.WriteLine("{0}. {1} {2} {3}", No, Nik, Nama, GB + (GB * 10 / 100));
+
+            if (GB < 0) {
+                Console.WriteLine("Maaf Gaji Kurang dari 0");
+            }
+        }
+    }
+}
